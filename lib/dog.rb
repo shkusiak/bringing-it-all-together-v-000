ffb8d4192bffaa466f14d64f66e796f1565fe37b
Shea Kusiak <shkusiak@gmail.com>
2018-06-29 00:33:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,36 @@
-class Dog
+class Dog 
   attr_accessor :name, :breed, :id
 
   def initialize(id=nil, name, breed)
-    @id = Marshal.load(Marshal.dump(obj_to_copy))
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+  end
+
+  def self.drop_table
+  end
+
+  def save
+  end
+
+  def self.create
+  end
+
+  def self.find_by_id
+  end
+
+  def self.find_or_create_by
+  end
+
+  def self.new_from_db
+  end
+
+  def self.find_by_name
+  end
+
+  def update
   end
 end
