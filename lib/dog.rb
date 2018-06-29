7545a4c3193e4488b494c72e00c44c9e577ccafb
Shea Kusiak <shkusiak@gmail.com>
2018-06-29 01:05:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,7 +59,11 @@ class Dog
   def self.find_or_create_by
   end
 
-  def self.new_from_db
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id,name,breed)
   end
 
   def self.find_by_name
