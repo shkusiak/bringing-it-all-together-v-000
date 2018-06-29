021968b3705cc3a699b997f1d6bc53f2ffa1d953
Shea Kusiak <shkusiak@gmail.com>
2018-06-29 01:11:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,7 +63,7 @@ class Dog
     id = row[0]
     name = row[1]
     breed = row[2]
-    self.new(id: id,name: name, breed:breed)
+    self.new(id: id, name: name, breed:breed)
   end
 
   def self.find_by_name
